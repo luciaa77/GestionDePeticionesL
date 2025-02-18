7f6351c2e134de37565b9d31c2d5addfd379a34d
luciaa77 <166419823+luciaa77@users.noreply.github.com>
2025-02-18 12:22:13 +0100

    Proyecto iniciado

---
+++ GestiónPeticiones/src/gestiónpeticiones/GestiónPeticiones.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestiónpeticiones;
+
+/**
+ *
+ * @author Etien & Ivan
+ */
+public class GestiónPeticiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int rol; 
+
+        do{
+            rol = modelo.Metodos.login();
+            if(rol == 0){
+                System.out.println("Usuario o contraseña invalido");                  
+            }else if(rol == 1){
+                System.out.println("Has accedido como usuario");
+            }else if (rol == 2){
+                System.out.println("Has accedido como administrador");
+                System.out.println();
+                System.out.println("___________________________");
+                
+                int opa = modelo.Metodos.menuAdministrador();
+                if(opa == 1){
+                    System.out.println();
+                    modelo.Metodos.imprimirPeticiones();
+                }else if(opa == 2){
+                    System.out.println();
+                    modelo.Metodos.eliminarTodasPeticiones();
+                }
+                
+            }
+        }while(rol == 0);
+    }
+    
+}
---
+++ GestiónPeticiones/src/modelo/Metodos.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Etien & Ivan
+ */
+public class Metodos {
+    
+    public static String[] peticiones = new String[100]; 
+
+    //Funcion login, devuelve 0 si contraseña o user sonn incorrectos
+    //Devuelve 1 si accedes como usuario normal
+    //Devuelve 2 si accedemos como administrador.
+    public static int login() {
+        int rol = 0;
+        Scanner sc = new Scanner(System.in);
+        String usuario_administrador = "lucia@";
+        String contraseña_administrador = "holaque123";
+        String usuarioInicial = "adrian3";
+        String contraseñaInicial = "adios456";
+
+        System.out.print("Nombre de usuario: ");
+        String usuario = sc.nextLine();
+        if (usuario.equals(usuario_administrador)) {
+            System.out.print("password: ");
+            String password = sc.nextLine();
+            if (password.equals(contraseña_administrador)) {
+                rol = 2;
+            }
+        } else if (usuario.equals(usuarioInicial)) {
+            System.out.print("password: ");
+            String password = sc.nextLine();
+            if (password.equals(contraseñaInicial)) {
+                rol = 1;
+            }
+        }
+        return rol;
+    }
+    
+    
+
+    public static int menuAdministrador() {
+        int opcion;
+        do {
+            System.out.println();
+            System.out.println("1. Ver todas las peticiones");
+            System.out.println("2. Eliminar todas las peticiones");
+            System.out.println("3. Ver una petición");
+            System.out.println("4. Eliminar una peticion");
+            System.out.println("5. Contestar una petición");
+            System.out.print("Introduce una opcion: ");
+            Scanner teclado = new Scanner(System.in);
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+            if(opcion > 5 || opcion < 1){
+                System.out.println("Opcion no encontrada.");
+            }
+        } while (opcion > 5 || opcion < 1);
+        return opcion;
+
+    }
+    
+    public static void imprimirPeticiones(){
+        int contador = 1;
+        for (int i = 0; i<peticiones.length;i++){
+            if(peticiones[i] != null){
+                System.out.println(contador + ". " + peticiones[i]);
+                contador++; 
+            }
+        }
+        if(contador == 1){
+            System.out.println("No hay peticiones");
+        }
+    }
+    
+    public static void eliminarTodasPeticiones(){
+        int contador = 1;
+        for (int i = 0; i<peticiones.length;i++){
+            if(peticiones[i] != null){
+                peticiones[i] = null; 
+                contador++; 
+            }
+        }
+        if(contador == 1){
+            System.out.println("No hay peticiones");
+        }
+    }
+
+}
