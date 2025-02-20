105142d6bfd597910dc830a3f6275261cdfad709
luciaa77 <166419823+luciaa77@users.noreply.github.com>
2025-02-20 13:00:30 +0100

    documentacion de algunos metodos

--- GestiónPeticiones/src/gestiónpeticiones/GestiónPeticiones.java
+++ GestiónPeticiones/src/gestiónpeticiones/GestiónPeticiones.java
@@ -7,89 +7,91 @@ package gestiónpeticiones;
 import java.util.Scanner;
 
 /**
+ * Programa para pedir peticiones o hacer pregutnas a un concesionario de coches
  *
- * @author Etien & Ivan
+ * @author Lucia Cuadrado
  */
 public class GestiónPeticiones {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        int rol = -1; 
-        String user = null; 
-        boolean terminado = true; 
-        while(terminado){
-            do{
-            
-            if(user == null){
-                Scanner sc = new Scanner(System.in);
-                System.out.print("Introduce el nombre de usuario: ");
-                user = sc.nextLine();
-                rol = modelo.Metodos.login(user);
-            }
-            if(rol == 0){
-                System.out.println("Usuario o contraseña invalido");                  
-            }else if(rol == 1){
-                System.out.println("Has accedido como usuario");
-                System.out.println();
-                System.out.println("___________________________");
-                
-                int opu = modelo.Metodos.menuUsuario();
-                
-                if(opu == 1){
+        int rol = -1;
+        String user = null;
+        boolean terminado = true;
+        while (terminado) {
+            do {
+
+                if (user == null) {
+                    Scanner sc = new Scanner(System.in);
+                    System.out.print("Introduce el nombre de usuario: ");
+                    user = sc.nextLine();
+                    rol = modelo.Metodos.login(user);
+                }
+                if (rol == 0) {
+                    System.out.println("Usuario o contraseña invalido");
+                } else if (rol == 1) {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("Has accedido como usuario");
                     System.out.println();
-                    boolean correcto = modelo.Metodos.escribirPeticion(user);
-                    if(correcto){
-                        System.out.println("Peticion insertada correctamente");
-                    }else{
-                        System.out.println("Ha ocurrido un error.");
+                    System.out.println("___________________________");
+
+                    int opu = modelo.Metodos.menuUsuario();
+
+                    if (opu == 1) {
+                        System.out.println();
+                        boolean correcto = modelo.Metodos.escribirPeticion(user);
+                        if (correcto) {
+                            System.out.println("Peticion insertada correctamente");
+                        } else {
+                            System.out.println("Ha ocurrido un error.");
+                        }
+                    } else if (opu == 2) {
+                        System.out.println();
+                        modelo.Metodos.verMisPeticiones(user);
+                    } else if (opu == 3) {
+                        user = null;
+                        rol = 0;
+                        System.out.println("Has cerrado sesion...");
+                        System.out.println();
+                    } else if (opu == 4) {
+                        terminado = false;
+                    } else if (opu == 5) {
+                        System.out.println();
+                        modelo.Metodos.verMisPeticiones(user);
+                        modelo.Metodos.eliminarPeticionPropia();
                     }
-                }else if(opu == 2){
+
+                } else if (rol == 2) {
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("Has accedido como administrador");
                     System.out.println();
-                    modelo.Metodos.verMisPeticiones(user);
-                }else if(opu == 3){
-                    user = null;
-                    rol = 0;
-                    System.out.println("Has cerrado sesion...");
-                    System.out.println();
-                }else if(opu == 4){
-                    terminado = false;
-                }else if(opu == 5){
-                    System.out.println();
-                    modelo.Metodos.verMisPeticiones(user);
-                    modelo.Metodos.eliminarPeticionPropia();
+                    System.out.println("___________________________");
+
+                    int opa = modelo.Metodos.menuAdministrador();
+                    if (opa == 1) {
+                        System.out.println();
+                        modelo.Metodos.imprimirPeticiones();
+                    } else if (opa == 2) {
+                        System.out.println();
+                        modelo.Metodos.eliminarTodasPeticiones();
+                    } else if (opa == 3) {
+                        System.out.println();
+                        modelo.Metodos.imprimirPeticiones();
+                        modelo.Metodos.eliminarPeticion();
+                    } else if (opa == 4) {
+                        user = null;
+                        rol = 0;
+                        System.out.println("Has cerrado sesion...");
+                        System.out.println();
+                    } else if (opa == 5) {
+                        terminado = false;
+                    }
+
                 }
-            
-            }else if (rol == 2){
-                System.out.println("Has accedido como administrador");
-                System.out.println();
-                System.out.println("___________________________");
-                
-                int opa = modelo.Metodos.menuAdministrador();
-                if(opa == 1){
-                    System.out.println();
-                    modelo.Metodos.imprimirPeticiones();
-                }else if(opa == 2){
-                    System.out.println();
-                    modelo.Metodos.eliminarTodasPeticiones();
-                }else if(opa == 3){
-                    System.out.println();
-                    modelo.Metodos.imprimirPeticiones();
-                    modelo.Metodos.eliminarPeticion();
-                }else if(opa == 4){
-                    user = null;
-                    rol = 0;
-                    System.out.println("Has cerrado sesion...");
-                    System.out.println();
-                }else if(opa == 5){
-                    terminado = false;
-                }
-                
-            }
-            
-        }while(rol == 0);
+
+            } while (rol == 0);
         }
     }
-    
+
 }
--- GestiónPeticiones/src/modelo/Metodos.java
+++ GestiónPeticiones/src/modelo/Metodos.java
@@ -1,23 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
 import java.util.Scanner;
 
 /**
+ * Metodos para realizar el programa
  *
- * @author Etien & Ivan
+ * @author Lucia Cuadrado
  */
 public class Metodos {
-    
-    public static String[] peticiones = new String[100]; 
-    public static String[] peticionesUser = new String[100]; 
 
-    //Funcion login, devuelve 0 si contraseña o user sonn incorrectos
-    //Devuelve 1 si accedes como usuario normal
-    //Devuelve 2 si accedemos como administrador.
+    public static String[] peticiones = new String[100];
+    public static String[] peticionesUser = new String[100];
+
+    /**
+     * Funcion login
+     *
+     * @param user
+     * @return 0 si da fallo, 1 si es usuario, 2 si es administrador
+     */
     public static int login(String user) {
         int rol = 0;
         Scanner sc = new Scanner(System.in);
@@ -30,8 +30,7 @@ public class Metodos {
         String u2 = "adrian2";
         String c2 = "adios456";
 
-        
-        String usuario = user; 
+        String usuario = user;
         if (usuario.equals(usuario_administrador)) {
             System.out.print("password: ");
             String password = sc.nextLine();
@@ -44,13 +43,13 @@ public class Metodos {
             if (password.equals(contraseñaInicial)) {
                 rol = 1;
             }
-        }else if (usuario.equals(u1)) {
+        } else if (usuario.equals(u1)) {
             System.out.print("password: ");
             String password = sc.nextLine();
             if (password.equals(c1)) {
                 rol = 1;
             }
-        }else if (usuario.equals(u2)) {
+        } else if (usuario.equals(u2)) {
             System.out.print("password: ");
             String password = sc.nextLine();
             if (password.equals(c2)) {
@@ -59,9 +58,12 @@ public class Metodos {
         }
         return rol;
     }
-    
-    
 
+    /**
+     * Para mostrar el menu del administrador
+     *
+     * @return
+     */
     public static int menuAdministrador() {
         int opcion;
         do {
@@ -75,14 +77,19 @@ public class Metodos {
             Scanner teclado = new Scanner(System.in);
             opcion = teclado.nextInt();
             teclado.nextLine();
-            if(opcion > 5 || opcion < 1){
+            if (opcion > 5 || opcion < 1) {
                 System.out.println("Opcion no encontrada.");
             }
         } while (opcion > 5 || opcion < 1);
         return opcion;
 
     }
-    
+
+    /**
+     * menu para mostrar usuario
+     *
+     * @return
+     */
     public static int menuUsuario() {
         int opcion;
         do {
@@ -90,105 +97,108 @@ public class Metodos {
             System.out.println("1. Insertar una petición");
             System.out.println("2. Ver mis peticiones");
             System.out.println("3. Cerrar Sesion");
-            System.out.println("4. Cerrar Programa");            
+            System.out.println("4. Cerrar Programa");
             System.out.println("5. Eliminar una de mis peticiones");
             System.out.print("Introduce una opcion: ");
             Scanner teclado = new Scanner(System.in);
             opcion = teclado.nextInt();
             teclado.nextLine();
-            if(opcion > 5 || opcion < 1){
+            if (opcion > 5 || opcion < 1) {
                 System.out.println("Opcion no encontrada.");
             }
         } while (opcion > 5 || opcion < 1);
         return opcion;
 
     }
-    
-    public static void eliminarPeticionPropia(){
+
+    /**
+     * metodo para eliminar una peticion de un usuario
+     */
+    public static void eliminarPeticionPropia() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Que peticion quieres borrar?");
         int posicion = sc.nextInt();
         sc.nextLine();
-        peticiones[posicion-1] = null;
-        peticionesUser[posicion-1] = null;
-        
+        peticiones[posicion - 1] = null;
+        peticionesUser[posicion - 1] = null;
+
     }
-    
-    public static boolean escribirPeticion (String user){
-        boolean correcto = false; 
-        Scanner teclado = new Scanner (System.in);
+
+    public static boolean escribirPeticion(String user) {
+        boolean correcto = false;
+        Scanner teclado = new Scanner(System.in);
         String pet;
         System.out.print("Introduce la peticion: ");
         pet = teclado.nextLine();
-        int contador = 0; 
-        for(int i = 0; i<peticiones.length; i++){
-            if(contador == 0){
-                if(peticiones[i] == null){
-                peticiones[i] = pet; 
-                peticionesUser[i] = user; 
-                correcto = true; 
-                contador++;
-            }
-            }
-            
-        }
-        return correcto; 
-    }
-    
-    public static void verMisPeticiones(String user){
-        int c = 0;
-        for(int i = 0; i<peticiones.length; i++){
-            if(peticionesUser[i] == user){
-                c++; 
-            }
-        }
-        if(c != 0){
-            System.out.println("Tus peticiones");
-            for(int i = 0; i<peticiones.length; i++){
-                if(peticionesUser[i] == user){
-                    System.out.println((i+1) + ". " + peticiones[i]);
+        int contador = 0;
+        for (int i = 0; i < peticiones.length; i++) {
+            if (contador == 0) {
+                if (peticiones[i] == null) {
+                    peticiones[i] = pet;
+                    peticionesUser[i] = user;
+                    correcto = true;
+                    contador++;
                 }
             }
-        }else{
+
+        }
+        return correcto;
+    }
+
+    public static void verMisPeticiones(String user) {
+        int c = 0;
+        for (int i = 0; i < peticiones.length; i++) {
+            if (peticionesUser[i] == user) {
+                c++;
+            }
+        }
+        if (c != 0) {
+            System.out.println("Tus peticiones");
+            for (int i = 0; i < peticiones.length; i++) {
+                if (peticionesUser[i] == user) {
+                    System.out.println((i + 1) + ". " + peticiones[i]);
+                }
+            }
+        } else {
             System.out.println("No hay peticiones");
         }
     }
-    
-    public static void imprimirPeticiones(){
+
+    public static void imprimirPeticiones() {
         int contador = 1;
-        for (int i = 0; i<peticiones.length;i++){
-            if(peticiones[i] != null){
+        for (int i = 0; i < peticiones.length; i++) {
+            if (peticiones[i] != null) {
                 System.out.println(contador + ". " + peticiones[i]);
-                contador++; 
+                contador++;
             }
         }
-        if(contador == 1){
+        if (contador == 1) {
             System.out.println("No hay peticiones");
         }
     }
-    
-    public static void eliminarTodasPeticiones(){
+
+    public static void eliminarTodasPeticiones() {
         int contador = 1;
-        for (int i = 0; i<peticiones.length;i++){
-            if(peticiones[i] != null){
-                peticiones[i] = null; 
+        for (int i = 0; i < peticiones.length; i++) {
+            if (peticiones[i] != null) {
+                peticiones[i] = null;
                 peticionesUser[i] = null;
-                contador++; 
+                contador++;
             }
         }
-        if(contador == 1){
+        if (contador == 1) {
             System.out.println("No hay peticiones");
         }
     }
-    
-    public static void eliminarPeticion(){
+
+    public static void eliminarPeticion() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Que peticion quieres borrar?");
         int posicion = sc.nextInt();
         sc.nextLine();
-        peticiones[posicion-1] = null;
-        peticionesUser[posicion-1] = null;
-        
+        peticiones[posicion - 1] = null;
+        peticionesUser[posicion - 1] = null;
+
     }
 
 }
